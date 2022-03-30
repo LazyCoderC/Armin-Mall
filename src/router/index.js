deef1fc7584e5ec76bcc99470bfad9b4ef457edc
LazyCoderC <396666417@qq.com>
2022-03-30 16:56:46 +0800

    在路由添加detail组件并封装routes数组

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import routes from './routes'
 
 Vue.use(VueRouter)
 
@@ -14,45 +15,6 @@ VueRouter.prototype.push = function (location, resolve, reject) {
   }
 }
 
-import Home from '@/pages/Home'
-import Search from '@/pages/Search'
-import Login from '@/pages/Login'
-import Register from '@/pages/Register'
-
 export default new VueRouter({
-  routes: [
-    {
-      path: '/home',
-      component: Home,
-      meta: {
-        show: true
-      }
-    },
-    {
-      path: '/search/:keyword?',
-      component: Search,
-      meta: {
-        show: true
-      },
-      name: 'search'
-    },
-    {
-      path: '/login',
-      component: Login,
-      meta: {
-        show: false
-      }
-    },
-    {
-      path: '/register',
-      component: Register,
-      meta: {
-        show: false
-      }
-    },
-    {
-      path: '/',
-      redirect: '/home'
-    }
-  ]
+  routes,
 })
---
+++ src/router/routes.js
@@ -0,0 +1,48 @@
+import Home from '@/pages/Home'
+import Search from '@/pages/Search'
+import Login from '@/pages/Login'
+import Register from '@/pages/Register'
+import Detail from '@/pages/Detail'
+
+export default [
+  {
+    path: '/home',
+    component: Home,
+    meta: {
+      show: true
+    }
+  },
+  {
+    path: '/search/:keyword?',
+    component: Search,
+    meta: {
+      show: true
+    },
+    name: 'search'
+  }, 
+  {
+    path: '/detail',
+    component: Detail,
+    meta: {
+      show: true
+    }
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {
+      show: false
+    }
+  },
+  {
+    path: '/register',
+    component: Register,
+    meta: {
+      show: false
+    }
+  },
+  {
+    path: '/',
+    redirect: '/home'
+  }
+]
