bcb4c623372c3a5ecd04d4b4a98d7eef39191d8d
LazyCoderC <396666417@qq.com>
2022-03-30 21:18:50 +0800

    zoom放大镜组件展示动态数据

--- src/pages/Detail/index.vue
+++ src/pages/Detail/index.vue
@@ -7,17 +7,22 @@
     <section class="con">
       <!-- 导航路径区域 -->
       <div class="conPoin">
-        <span>手机、数码、通讯</span>
-        <span>手机</span>
-        <span>Apple苹果</span>
-        <span>iphone 6S系类</span>
+        <span v-show="categoryView.category1Name">{{
+          categoryView.category1Name
+        }}</span>
+        <span v-show="categoryView.category2Name">{{
+          categoryView.category2Name
+        }}</span>
+        <span v-show="categoryView.category3Name">{{
+          categoryView.category3Name
+        }}</span>
       </div>
       <!-- 主要内容区域 -->
       <div class="mainCon">
         <!-- 左侧放大镜区域 -->
         <div class="previewWrap">
           <!--放大镜效果-->
-          <Zoom />
+          <Zoom :skuImageList="skuImageList" />
           <!-- 小图列表 -->
           <ImageList />
         </div>
@@ -25,10 +30,10 @@
         <div class="InfoWrap">
           <div class="goodsDetail">
             <h3 class="InfoName">
-              Apple iPhone 6s（A1700）64G玫瑰金色 移动通信电信4G手机
+              {{ skuInfo.skuName }}
             </h3>
             <p class="news">
-              推荐选择下方[移动优惠购],手机套餐齐搞定,不用换号,每月还有花费返
+              {{ skuInfo.skuDesc }}
             </p>
             <div class="priceArea">
               <div class="priceArea1">
@@ -37,7 +42,7 @@
                 </div>
                 <div class="price">
                   <i>¥</i>
-                  <em>5299</em>
+                  <em>{{ skuInfo.price }}</em>
                   <span>降价通知</span>
                 </div>
                 <div class="remark">
@@ -349,6 +354,8 @@
 <script>
 import ImageList from "./ImageList/ImageList";
 import Zoom from "./Zoom/Zoom";
+// 获取vuex仓库数据
+import { mapGetters } from "vuex";
 
 export default {
   name: "Detail",
@@ -357,8 +364,15 @@ export default {
     Zoom,
   },
   mounted() {
-    this.$store.dispatch("getGoodInfo", this.$route.params.skuId)
-  }
+    this.$store.dispatch("getGoodInfo", this.$route.params.skuId);
+  },
+  computed: {
+    ...mapGetters(["categoryView", "skuInfo"]),
+    // 处理数据，确保如果数据没有请求回来的情况下不为undefined
+    skuImageList() {
+      return this.skuInfo.skuImageList || [];
+    },
+  },
 };
 </script>
 
--- src/store/detail/index.js
+++ src/store/detail/index.js
@@ -18,7 +18,14 @@ const actions = {
     }
   }
 }
-const getters = {}
+const getters = {
+  categoryView(state) {
+    return state.goodInfo.categoryView || {}
+  },
+  skuInfo(state) {
+    return state.goodInfo.skuInfo || {}
+  }
+}
 export default {
   state,
   mutations,
